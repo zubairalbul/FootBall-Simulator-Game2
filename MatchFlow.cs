dd433d9afea6eeee8fbb36cc425378c3a34e90c8
Zubair Al Bulushi <zubair.albu@gmial.com>
2024-10-10 11:59:00 +0400

    for matchflow class inetialized two teams and their attributes

--- MatchFlow.cs
+++ MatchFlow.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace FootBall_Simulator_Game2
 {
-    internal class MatchFlow
+    public class MatchFlow
     {
+        private ITeam team1;
+        private ITeam team2;
+        private Random random = new Random();
+
+        // Initialize with two teams
+        public MatchFlow(ITeam team1, ITeam team2)
+        {
+            this.team1 = team1;
+            this.team2 = team2;
+        }
     }
 }
