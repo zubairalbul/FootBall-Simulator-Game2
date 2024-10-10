1e54c65887bef7d97795927174a91b5ea8ab4123
Zubair Al Bulushi <zubair.albu@gmial.com>
2024-10-10 10:26:08 +0400

    Added a Contractor for the class and made the list public since it was showing error and fixed it.

--- Team.cs
+++ Team.cs
@@ -11,7 +11,14 @@ namespace FootBall_Simulator_Game2
         //Team's Name and List of Players:
         public string Name { get; private set; }
         public int Score { get; private set; }
-        List<IPlayer> players { get; private set; }= new List<IPlayer>();
+        public List<IPlayer> players { get; private set; }= new List<IPlayer>();
+        
+        public Team(string name , int score, List<IPlayer> players)
+        {
+            Name = name;
+            Score = score;
+            this.players = players;
+        }// Constructor to initialize the team with a name
     }
 }
     
