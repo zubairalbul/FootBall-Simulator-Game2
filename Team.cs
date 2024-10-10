272d4918a8fdadf2df5f3af5da41d43319bc3e35
Zubair Al Bulushi <zubair.albu@gmial.com>
2024-10-10 10:38:26 +0400

    New function added to Create players and notced that when giving these players positions we need an list or anything to store it.

--- Team.cs
+++ Team.cs
@@ -19,6 +19,18 @@ namespace FootBall_Simulator_Game2
             Score = score;
             this.players = players;
         }// Constructor to initialize the team with a name
+
+        private void GeneratePlayers() // This function is for Generating Players with names and positioning them.
+        {
+            // Predefined positions for players
+            string[] positions = { "Forward", "Midfielder", "Defender", "Goalkeeper" };
+            // Create 11 players: 3 Forwards, 4 Midfielders, 3 Defenders, and 1 Goalkeeper
+            for (int i = 0; i < 11; i++) 
+            {
+            string positioning= positions[i < 3 ? 0 : i < 7 ? 1 : i < 10 ? 2 : 3];
+                players.Add(playerFactory.CreatePlayer(positioning));//noticed we need an list to store the positions of the players.
+            }
+        }
     }
 }
     
