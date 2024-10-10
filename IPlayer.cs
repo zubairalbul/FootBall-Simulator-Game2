e35b346f554610a3faf70654f6ac7e808cb196c0
Zubair Al Bulushi <zubair.albu@gmial.com>
2024-10-10 09:43:56 +0400

    creating an interface which will have the properties of the players since every player going the same attributes (name, Position, and Skill).

--- IPlayer.cs
+++ IPlayer.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace FootBall_Simulator_Game2
 {
-    internal interface IPlayer
+    public interface IPlayer
     {
+        string Name { get; }
+        string Position { get; }
+        int Skill {  get; }
     }
 }
