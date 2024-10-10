90ca938db40ed689654458fb57cfe8702b4e812d
Zubair Al Bulushi <zubair.albu@gmial.com>
2024-10-10 10:03:14 +0400

    Intializing Properties of the Players in the class whic has been inherted from IPlayer Interface.

--- Players.cs
+++ Players.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace FootBall_Simulator_Game2
 {
-    internal class Players
+    public class Players : IPlayer
     {
+        // Properties for player name, position, and skill
+        public string Name { get; private set; }
+        public string Position { get; private set; }
+        public int Skill {  get; private set; }
+        // Constructor to initialize player with a name, position, and skill
+        
     }
 }
