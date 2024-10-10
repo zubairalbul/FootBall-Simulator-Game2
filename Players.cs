5b5670d1b4b4f51b2351f8d909c34a664b3476a1
Zubair Al Bulushi <zubair.albu@gmial.com>
2024-10-10 09:40:09 +0400

    Added Reqired Classes And Interfaces which going to have the most program implementation requirements to run, more will may added if needed

---
+++ Players.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootBall_Simulator_Game2
+{
+    internal class Players
+    {
+    }
+}
