9508172649a81832820ea0b40c7f14e5e341feb0
Zubair Al Bulushi <zubair.albu@gmial.com>
2024-10-10 12:45:19 +0400

    Created an Needed class to assign names randomly to players however this class is inherted from interface IPlayerGenerator us a reference.

---
+++ CreatePlayers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootBall_Simulator_Game2
+{
+    public class CreatePlayers : IPlayerGenerator
+    {
+        private Random random = new Random();
+        private readonly List<string> PlayerNames = new List<string>()
+        {
+        "Ahmed", "Omar", "Hassan", "Ali", "Mohammed", "Youssef", "Mahmoud", "Khaled", "Ibrahim", "Said",
+        "Ziad", "Tariq", "Fahad", "Musa", "Salim", "Rami", "Samir", "Jamil", "Nasser", "Mustafa"
+        };
+
+
+    }
+}
+
